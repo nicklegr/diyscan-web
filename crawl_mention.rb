7257b5ffc49b664d500516c43712fa70a19138ce
nicklegr <1327502+nicklegr@users.noreply.github.com>
2020-06-21 16:56:47 +0900

    未処理のツイートをタスクキューに追加

--- crawl_mention.rb
+++ crawl_mention.rb
@@ -2,6 +2,7 @@
 
 require 'pp'
 require 'yaml'
+require 'securerandom'
 require 'twitter'
 require_relative 'db'
 
@@ -25,10 +26,27 @@ loop do
     client.mentions_timeline(option).each do |tweet|
       # puts "mention: from #{tweet.user.id} (@#{tweet.user.screen_name}): #{tweet.text[0...30]}"
       # pp tweet.to_hash
-      if tweet.attrs[:extended_entities]
-        video = tweet.attrs[:extended_entities][:media].select{|e| e[:type] == "video"}.first
-        video_url = video[:video_info][:variants].select{|e| e[:content_type] == "video/mp4"}.max_by{|e| e[:bitrate]}[:url]
-        puts video_url
+      next if !tweet.attrs[:extended_entities]
+
+      video = tweet.attrs[:extended_entities][:media].select{|e| e[:type] == "video"}.first
+      video_url = video[:video_info][:variants].select{|e| e[:content_type] == "video/mp4"}.max_by{|e| e[:bitrate]}[:url]
+      # puts video_url
+
+      if Task.where(twitter_status_id: tweet.id).count == 0
+        job_id = SecureRandom.uuid
+        puts "add_task: #{job_id}, status_id: #{tweet.id}"
+        Task.create!(
+          job_id: job_id,
+          status: "waiting",
+          requested_at: tweet.created_at,
+          finished_at: nil,
+          movie_url: video_url,
+          ocr_result: nil,
+          twitter_user_id: tweet.user.id,
+          twitter_screen_name: tweet.user.screen_name,
+          twitter_status_id: tweet.id,
+          twitter_raw_tweet: tweet.to_hash,
+          )
       end
     end
   rescue => e
@@ -37,5 +55,6 @@ loop do
 
   STDOUT.flush
   sleep(15)
-  # sleep(600)
+# sleep(600)
+# break
 end
--- db.rb
+++ db.rb
@@ -15,6 +15,29 @@ class Result
   index({ key: 1 }, { unique: true, name: "key_index" })
 end
 
+class Task
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :job_id, type: String
+  field :status, type: String # waiting, processing, done
+  field :requested_at, type: Time
+  field :finished_at, type: Time
+
+  field :movie_url, type: String
+  field :ocr_result, type: String
+
+  field :twitter_user_id, type: Integer
+  field :twitter_screen_name, type: String
+  field :twitter_status_id, type: Integer
+  field :twitter_raw_tweet, type: Hash
+
+  index({ twitter_status_id: 1 }, { unique: true, name: "twitter_status_id_index" })
+  index({ status: 1 }, { name: "status_index" })
+  index({ request_time: 1 }, { name: "request_time_index" })
+end
+
 Mongoid.load!("mongoid.yml", :production)
 
 Result.create_indexes
+Task.create_indexes
