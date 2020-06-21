dfe361f9b5799acd40af722013e1a46683a16cc9
nicklegr <1327502+nicklegr@users.noreply.github.com>
2020-06-21 13:26:39 +0900

    クラス化

--- build_recipe_list.rb
+++ build_recipe_list.rb
@@ -34,64 +34,64 @@ require "pp"
 require "json"
 require "damerau-levenshtein"
 
-$recipe_names = File.readlines("recipe_list.txt").map(&:rstrip).freeze
-
-def recipe_names
-  $recipe_names
-end
-
-def recipe_chars
-  recipe_names.join.chars.uniq.sort.join
-end
-
-def recipe_char_regex
-  /[#{recipe_chars}]+/
-end
-
-def cleanup_str(str)
-  # 表記ゆれしそうな文字を正規化
-  str.tr("０-９ａ-ｚＡ-Ｚ", "0-9a-zA-Z")
-    .scan(recipe_char_regex)
-    .join("")
-end
-
-def distance(str1, str2)
-  # ダメラウ・レーベンシュタイン距離は文字の入れ替えも行う
-  # 純粋なレーベンシュタイン距離は入れ替えを行わない
-  # OCRなので、文字の入れ替えは発生しにくいと想定して後者を使う
-  block_size = 0
-  max_distance = 10
-  DamerauLevenshtein.distance(str1, str2, block_size, max_distance)
-end
-
-def nearest_recipe_name(str)
-  cand = recipe_names.min_by do |e|
-    distance(str, e)
+class ResultParser
+  def initialize
+    @recipe_names = File.readlines("recipe_list.txt").map(&:rstrip).freeze
+    @recipe_chars = @recipe_names.join.chars.uniq.sort.join.freeze
+    @recipe_char_regex = /[#{@recipe_chars}]+/.freeze
   end
 
-  d = distance(str, cand)
+  def parse(json)
+    data = JSON.parse(json)
+    list = data.chunk{|e| e["detected"]}.map do |detected, frames|
+      if detected
+        frames.map do |e|
+          clean_str = cleanup_str(e["text"])
+          puts "#{clean_str} -> #{nearest_recipe_name(clean_str)}"
 
-  # 編集で全然別の名前に変わってるものは却下
-  if d >= str.size / 2
-    nil
-  else
-    cand
-  end
-end
-
-data = JSON.parse(File.read("test.json"))
-
-list = data.chunk{|e| e["detected"]}.map do |detected, frames|
-  if detected
-    frames.map do |e|
-      clean_str = cleanup_str(e["text"])
-      puts "#{clean_str} -> #{nearest_recipe_name(clean_str)}"
-
-      nearest_recipe_name(clean_str)
+          nearest_recipe_name(clean_str)
+        end
+      else
+        nil
+      end
     end
-  else
-    nil
+
+    list
+  end
+
+  private
+
+  def cleanup_str(str)
+    # 表記ゆれしそうな文字を正規化
+    str.tr("０-９ａ-ｚＡ-Ｚ", "0-9a-zA-Z")
+      .scan(@recipe_char_regex)
+      .join("")
+  end
+
+  def distance(str1, str2)
+    # ダメラウ・レーベンシュタイン距離は文字の入れ替えも行う
+    # 純粋なレーベンシュタイン距離は入れ替えを行わない
+    # OCRなので、文字の入れ替えは発生しにくいと想定して後者を使う
+    block_size = 0
+    max_distance = 10
+    DamerauLevenshtein.distance(str1, str2, block_size, max_distance)
+  end
+
+  def nearest_recipe_name(str)
+    cand = @recipe_names.min_by do |e|
+      distance(str, e)
+    end
+
+    d = distance(str, cand)
+
+    # 編集で全然別の名前に変わってるものは却下
+    if d >= str.size / 2
+      nil
+    else
+      cand
+    end
   end
 end
 
-pp list
+parser = ResultParser.new
+pp parser.parse(File.read("test.json"))
